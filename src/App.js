c576842745ef7fbe1377ccef0c8938dcb430b718
Thainguyen3003 <nguyennangthai@gmail.com>
2022-04-28 22:31:12 +0700

    up code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Navbar from "./Components/Navbar/Navbar";
+import Intro from "./Components/Intro/Intro";
 import './App.css'
+
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro />
     </div>
   );
 }
